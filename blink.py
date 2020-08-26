54b8029056a17e0f8aa052cc3cc2cf1089c51c52
russellshome <russellshome@gmail.com>
2020-08-26 20:08:02 +1200

    Added comments

--- blink.py
+++ blink.py
@@ -1,7 +1,16 @@
+#!/usr/bin/env python
+"""
+Description : Blink an LED
+Author      : Russell
+E-mail      : russellshome@gmail.com
+Date        : 2020/08/26
+Circuit     : https://crcit.net/c/98db0b1c4bcc420fbb3f02dd2a52553a
+"""
 from gpiozero import LED
 from time import sleep
 
 led = LED(17)
+print(__doc__)
 
 while True:
     led.on()
--- button.py
+++ button.py
@@ -1,4 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+"""
+Description : Press button to make LED flash
+Author      : Russell
+E-mail      : russellshome@gmail.com
+Date        : 2020/08/26
+Circuit     : https://crcit.net/c/6d7e54db10d4410b9e11d9e287187565
+"""
 from gpiozero import LED, Button
 from time import sleep
 from signal import pause
